ee9f0e7c8a23d2b3cb2dd4502a6fabe1f34a6876
kawser2917 <kawserahmed8492@gmail.com>
2025-02-18 23:18:51 +0600

    button toggle done with js

--- js/home.js
+++ js/home.js
@@ -12,17 +12,3 @@ document.getElementById("btn-add-money").addEventListener('click',function(event
         alert("Something went wrong.. Please try again")
     }
 })
-// Cash out
-document.getElementById("btn-cash-out").addEventListener('click',function(event){
-    event.preventDefault();
-    const amount = document.getElementById('cash-out-amount').value;
-    const pinNumber = document.getElementById('cash-pin-number').value;
-    if(pinNumber==="1234"){
-        const balance = document.getElementById("balance").innerText;
-        const newBalance = parseFloat(balance) - parseFloat(amount);
-        document.getElementById("balance").innerText = newBalance;
-    }
-    else{
-        alert("Something went wrong.. Please try again")
-    }
-})
